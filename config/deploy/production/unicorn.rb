9a82bc5a5622319079a8aa96a240e9fb7013f64d
jason <jason.addie@gmail.com>
2017-06-14 18:07:39 +0400

    bug fix - unicorn app name

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,8 +1,8 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/deploy/Bootstrap-Starter/current" # path to application current folder
-sock_name = "unicorn_Bootstrap-Starter" # must be unique name with no '.'
+root = "/home/deploy/Hours/current" # path to application current folder
+sock_name = "unicorn_Hours" # must be unique name with no '.'
 port_num = 8091 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
