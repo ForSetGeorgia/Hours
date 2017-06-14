c5996d6d69502ab2594e282a04cba131d6f5f236
jason <jason.addie@gmail.com>
2017-06-14 18:01:06 +0400

    update deploy; fix gemfile

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -1,7 +1,7 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/bootstrap/Bootstrap-Starter/current" # path to application current folder
+root = "/home/deploy/Bootstrap-Starter/current" # path to application current folder
 sock_name = "unicorn_Bootstrap-Starter" # must be unique name with no '.'
 port_num = 8091 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
